b1faffca6290b5f70baaebd6838c448ef7a928a0
idDxDbi <66623647+idDxDbi@users.noreply.github.com>
2022-12-12 12:03:24 +0800

    Create teat.py

---
+++ teat.py
@@ -0,0 +1,19 @@
+import numpy
+
+import cv2
+
+
+
+webcom = True
+
+class fun():
+    def test(self):
+        print("hello world!")
+
+
+if __name__ == "__main__":
+
+    i = fun()
+
+    i.test()
+
